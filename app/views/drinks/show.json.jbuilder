954d8b997b5efc0204b3e997683bca1c05a22427
jacqueline-homan <jacquelinehoman7@gmail.com>
2013-12-12 20:38:41 -0500

    Getting that sticker

---
+++ app/views/drinks/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @drink, :id, :name, :temperature, :created_at, :updated_at
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Sticker::Application.config.secret_key_base = 'd99807e92be5e1cccbbf2a0d5231e97ff4d4453cca42dc2c0e08eda4ab33abc545c6709cbf4965e7e4b0b0050f1c17b11b048222b3d6f14ec08b9014f7b7d6b0'
